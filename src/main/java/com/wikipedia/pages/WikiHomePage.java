7e586ceeff848fc3b675e25f0d5b6627311572a2
Khaled.Roushdy <Khaled.Roushdy@is-lt-7918.itworx.com>
2017-04-18 13:38:21 +0200

    Update the home Page

--- src/main/java/com/wikipedia/pages/WikiHomePage.java
+++ src/main/java/com/wikipedia/pages/WikiHomePage.java
@@ -3,4 +3,12 @@ package com.wikipedia.pages;
 public class WikiHomePage {
 
 	//TODO Add the Elements here
+	// The elements should be inspected with UIAutomation tool 
+	
+	public void Search(String searchData){
+		
+		//Inspect the search Field
+	}
+	
+	
 }
